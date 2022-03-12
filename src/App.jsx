c256ebcba857914f950d05ac58c174e8feac1ec2
jhoan sebastian <jhoansebax257@gmail.com>
2022-03-11 21:21:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// Packages
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Components
+import Layout from './layout/Layout'
+import Inicio from './pages/Inicio'
+import NuevoCliente from './pages/NuevoCliente'
+import EditarCliente from './pages/EditarCliente'
+import VerCliente from './pages/VerCliente'
+
+function App() {
+ 
+
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/clientes" element={<Layout />}>
+          <Route index  element={<Inicio />}/>
+          <Route path='nuevo' element={<NuevoCliente/>}/>
+          <Route path='editar/:id' element={<EditarCliente/>}/>
+          <Route path=':id' element={<VerCliente/>}/>
+        </Route>
+
+      </Routes>    
+    </BrowserRouter>
+  )
+}
+
+export default App
